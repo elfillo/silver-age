336fb4f957c248ccd382b9815a5e4c4de313970b
elf <filonenko0406@gmail.com>
2020-03-06 20:27:05 +0800

    fix (docs)

--- docs.php
+++ docs.php
@@ -4,4 +4,9 @@
 	*/
 ?>
 <?php get_header()?>
+<section class="page">
+	<div class="container">
+		<?php echo apply_filters('the_content', $post->post_content);?>
+	</div>
+</section>
 <?php get_footer()?>
