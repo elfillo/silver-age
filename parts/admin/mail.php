8bbc4710187062a0f6288a8a9a2dacabffd7dfc1
elf <filonenko0406@gmail.com>
2020-03-16 20:25:18 +0800

    fix email

--- parts/admin/mail.php
+++ parts/admin/mail.php
@@ -7,7 +7,7 @@ function getBookingForm(){
 	$newLine = "<br/>";
 
 	$headers  = "Content-type: text/html; charset=utf-8 \r\n";
-	//$headers .= "From: От кого письмо <from@example.com>\r\n";
+	$headers .= "From: Серебряный Век <from@example.com>\r\n";
 	$headers .= "Reply-To: '.$sendTo.'\r\n";
 
 	$to      = $sendTo;
